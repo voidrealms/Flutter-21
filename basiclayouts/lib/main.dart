68e427b376a056afe63612760d5a197afda89524
Rootshell <bcairns@voidrealms.com>
2017-12-08 11:50:43 -0500

    first commit

---
+++ basiclayouts/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Basic Layouts',
+      home: new Scaffold(
+        appBar: new AppBar(title: new Text('Basic Layouts'),),
+        body: new Container(
+          padding: const EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text('hello'),
+                new Text('world'),
+                new Text('!!!'),
+                new Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Container(
+                      padding: const EdgeInsets.all(5.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('hello'),
+                          new Text('world'),
+                          new Text('!!!'),
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.all(5.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('hello'),
+                          new Text('world'),
+                          new Text('!!!'),
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.all(5.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('hello'),
+                          new Text('world'),
+                          new Text('!!!'),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+Code for video tutorial
+
+Title: Flutter 21 - MaterialApp
+Published: 12/8/2017 11:45 AM
+Link: https://youtu.be/H72MnhPzJQs
+
+Description: Learn how to use rows and columns in flutter
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
